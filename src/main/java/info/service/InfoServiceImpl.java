0891b5f3618d0b4e1f728711c31a7cfaeace7c48
jinuk93 <rlawlsdnr03@naver.com>
2021-11-25 17:39:54 +0900

    jinuk

--- src/main/java/info/service/InfoServiceImpl.java
+++ src/main/java/info/service/InfoServiceImpl.java
@@ -1,8 +1,23 @@
 package info.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import grade.bean.GradeDTO;
+import info.bean.InfoDTO;
+import info.dao.InfoDAO;
 
 @Service
 public class InfoServiceImpl implements InfoService {
+	@Autowired
+	private InfoDAO infoDAO;
+
+	@Override
+	public List<GradeDTO> getBenefit() {
+		// TODO Auto-generated method stub
+		return infoDAO.getBenefit();
+	}
 
 }
