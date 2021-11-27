915497caf00c977b3bdf03416620a568148348cc
e9696 <steam0410@naver.com>
2021-11-27 21:21:49 +0900

    권영민 수 정

--- src/main/java/member/controller/MemberWriteController.java
+++ src/main/java/member/controller/MemberWriteController.java
@@ -27,7 +27,7 @@ public class MemberWriteController {
 	@Autowired
 	private JavaMailSender mailSender;
 	@Autowired
-	private BCryptPasswordEncoder passwordEncoder;
+	private BCryptPasswordEncoder bcryptPasswordEncoder;
 
 	@GetMapping(value="/memberAgree")
 	public String writeAgreeForm(Model model) {
@@ -103,28 +103,10 @@ public class MemberWriteController {
 	@RequestMapping(value="/write", method=RequestMethod.POST)
 	@ResponseBody
 	public void write(@ModelAttribute MemberDTO memberDTO) {
+		System.out.println("암호화 되기 전 : " + memberDTO.getPwd());
+		memberDTO.setPwd(bcryptPasswordEncoder.encode(memberDTO.getPwd()));
+		System.out.println("암호화 됟ㄴ 후 : " + memberDTO.getPwd());
 		memberWriteService.write(memberDTO);
 	}
 	
-	@RequestMapping(value = "/secuTest", method = RequestMethod.GET)
-    public void secuTest() {
-        
-        String rawPassword = "vam123";                //인코딩 전 메서드
-        String encdoePassword1;                        // 인코딩된 메서드
-        String encdoePassword2;                        // 똑같은 비밀번호 데이터를 encdoe()메서드를 사용했을 때 동일한 인코딩된 값이 나오는지 확인하기 위해 추가
-        
-        encdoePassword1 = passwordEncoder.encode(rawPassword);
-        encdoePassword2 = passwordEncoder.encode(rawPassword);
-        
-        // 인코딩된 패스워드 출력
-        System.out.println("encdoePassword1 : " +encdoePassword1);
-        System.out.println(" encdoePassword2 : " + encdoePassword2);
-        
-        String truePassowrd = "vam123";
-        String falsePassword = "asdfjlasf";
-        
-        System.out.println("truePassword verify : " + passwordEncoder.matches(truePassowrd, encdoePassword1));
-        System.out.println("falsePassword verify : " + passwordEncoder.matches(falsePassword, encdoePassword1));    
-    
-    }
 }
