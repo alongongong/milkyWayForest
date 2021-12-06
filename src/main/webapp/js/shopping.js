f5700ace74cc6035c0cabecbf2dd595d12925716
jinuk93 <rlawlsdnr03@naver.com>
2021-12-07 00:18:03 +0900

    chatbot

--- src/main/webapp/js/shopping.js
+++ src/main/webapp/js/shopping.js
@@ -1,16 +1,5 @@
 $(function(){
 
-	$('#plus').click(function(){
-		$('.length input').val(parseInt($('.length input').val())+1);
-	});
-	
-	$('#minus').click(function(){
-		
-		$('.length input').val(parseInt($('.length input').val())-1);
-		if($('.length input').val() < 1) {
-			$('.length input').val(1);
-		}		
-	});
 });
 			
 
