71ed75c923296e82078d54922214154df26cb09a
alongongong <al_ong@naver.com>
2021-12-12 22:58:41 +0900

    header slide 끝!

--- src/main/webapp/js/header.js
+++ src/main/webapp/js/header.js
@@ -2,9 +2,10 @@ $(function(){
 	$('#Allheader .menu a').mouseenter(function(){
 		$('#headerSection').stop().animate({height: '450'}, 500);
 		
-		$('#headerSection').css('background-color', 'rgba(255,255,255,0.8)');
+		$('#headerSection').css('background-color', 'rgba(255,255,255,0.95)');
 		$('#headerSection').css('transition-property', 'background-color');
 		$('#headerSection').css('transition-duration', '0.5s');
+		$('#headerSection').css('box-shadow', '3px 0 10px rgba(120,120,120,0.1)');
 		
 		$('.menu > li').css('padding', '0 1rem 0 0');
 		$('.menu > li').css('transition-property', 'padding');
@@ -20,7 +21,6 @@ $(function(){
 		$('#Allheader header > div .spot li a').css('transition-property', 'font-weight, font-size');
 		$('#Allheader header > div .spot li a').css('transition-duration', '0.5s');
 		
-		$('#Allheader header ul li a').css('transition', 'all .3s cubic-bezier(0.16, 0.04, 0.03, 0.95)');
 		
 		
 		$('#headerSection').hover(function(){
@@ -31,21 +31,22 @@ $(function(){
 			$('#headerSection').css('background-color', 'rgba(255,255,255,0.5)');
 			$('#headerSection').css('transition-property', 'background-color');
 			$('#headerSection').css('transition-duration', '1s');
+			$('#headerSection').css('box-shadow', 'none');
 			
 			$('.menu > li').css('padding', '0');
 			$('.menu > li').css('transition-property', 'padding');
-			$('.menu > li').css('transition-duration', '0.5s');
+			$('.menu > li').css('transition-duration', '1s');
 				
 			$('.menu > li > a').css('color', '#444');
 			$('.menu > li > a').css('font-size', '.9rem');
 			$('.menu > li > a').css('transition-property', 'color, font-size');
-			$('.menu > li > a').css('transition-duration', '0.5s');
+			$('.menu > li > a').css('transition-duration', '1s');
 			
 			$('#Allheader header > div .spot li a').css('font-size', '.7rem');
 			$('#Allheader header > div .spot li a').css('font-weight', 'bold');
 			$('#Allheader header > div .spot li a').css('transition-property', 'font-weight, font-size');
 			$('#Allheader header > div .spot li a').css('transition-duration', '0.5s');
-		
+
 		});
 	});
 });
